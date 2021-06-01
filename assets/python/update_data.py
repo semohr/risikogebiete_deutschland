0c89086103ce110d280e94f1245469deb5573ddd
Sebastian <sebastian@mohrenclan.de>
2021-06-01 10:47:31 +0200

    Small bugfix and cleanup.

--- assets/python/update_data.py
+++ assets/python/update_data.py
@@ -3,7 +3,7 @@
 # @Author:        Sebastian B. Mohr
 # @Email:
 # @Created:       2020-10-15 14:08:49
-# @Last Modified: 2021-04-15 15:37:34
+# @Last Modified: 2021-06-01 10:46:29
 # ------------------------------------------------------------------------------ #
 import ujson
 import pandas as pd
@@ -19,10 +19,8 @@ rki.download_all_available_data(force_download=True)
 
 
 # Load population data
-population = pd.read_csv(
-    "../data/population_rki_age_groups.csv", encoding="cp1252",
-)
-
+population = pd.read_csv("../data/population_rki_age_groups.csv", encoding="cp1252",)
+population["ags"] = population["ags"].dropna().astype(int)
 
 # FORMAT BS Should not be neccesary anymore:
 """
@@ -78,6 +76,13 @@ for lk_id in data_cases.index.get_level_values(level="IdLandkreis").unique():
     if lk_id == 5354:
         continue
 
+    if lk_id == 2000:
+        lk_id_pop = 2
+    elif lk_id == 11001:
+        lk_id_pop = 11000
+    else:
+        lk_id_pop = lk_id
+
     # BERLIN
     berlin = [
         11002,
@@ -108,6 +113,11 @@ for lk_id in data_cases.index.get_level_values(level="IdLandkreis").unique():
     # Normal
     data[lk_id] = {}
     data[lk_id]["weekly_cases"] = data_lk.sum()
+    try:
+        pop_lk = pop_rki_aligned.xs(lk_id_pop, level="ags")
+    except:
+        print(f"{lk_id}, {lk_id_pop}")
+        continue
 
     try:
         data[lk_id]["inzidenz"] = (
@@ -117,28 +127,18 @@ for lk_id in data_cases.index.get_level_values(level="IdLandkreis").unique():
         )
     except:
         data[lk_id]["inzidenz"] = (
-            data[lk_id]["weekly_cases"] * 100000 / population_landkreise[str(lk_id)]
+            data[lk_id]["weekly_cases"]
+            * 100000
+            / (pop_lk.sum(axis=1).values[0] - pop_lk["unbekannt"].values[0])
         )
 
     # Age groups
     for ag in data_lk.index:
         data[lk_id][f"weekly_cases_{ag}"] = data_lk[ag]
         try:
-            data[lk_id][f"inzidenz_{ag}"] = (
-                data_lk[ag]
-                * 100000
-                / pop_rki_aligned[ag].xs(lk_id, level="ags").values[0]
-            )
+            data[lk_id][f"inzidenz_{ag}"] = data_lk[ag] * 100000 / pop_lk[ag].values[0]
         except:
-            data[lk_id][f"inzidenz_{ag}"] = (
-                data_lk[ag]
-                * 100000
-                / (
-                    pop_rki_aligned.sum()[ag]
-                    / pop_rki_aligned.sum().sum()
-                    * population_landkreise[str(lk_id)]
-                )
-            )
+            data[lk_id][f"inzidenz_{ag}"] = data_lk[ag] * 100000 / pop_lk[ag].values[0]
 
 # Write file
 with open("../data/data_latest.json", "w") as outfile:
