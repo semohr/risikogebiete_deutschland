8997522583bed0081a57aa21438ed0ec6408e8a3
Sebastian Mohr <sebastian@mohrenclan.de>
2021-11-24 00:34:07 +0100

    Working d3 plotting with our geojson

---
+++ assets/js/utils.js
@@ -0,0 +1,17 @@
+// Project the data using Proj4
+function project(geojson, projection) {
+    const projectPolygon = coordinate => {
+        coordinate.forEach((lonLat, i) => {
+            coordinate[i] = window.proj4(projection, lonLat);
+        });
+    };
+    geojson.features.forEach(function (feature) {
+        if (feature.geometry.type === 'Polygon') {
+            feature.geometry.coordinates.forEach(projectPolygon);
+        } else if (feature.geometry.type === 'MultiPolygon') {
+            feature.geometry.coordinates.forEach(items => {
+                items.forEach(projectPolygon);
+            });
+        }
+    });
+}
